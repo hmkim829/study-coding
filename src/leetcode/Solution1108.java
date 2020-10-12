b5ae51c58e93073b2cc6db854840cc8c927241ae
hmkim829 <hmkim829@gmail.com>
2020-10-12 22:44:40 +0900

    1374. Generate a String With Characters That Have Odd Counts

---
+++ src/leetcode/Solution1108.java
@@ -0,0 +1,28 @@
+package leetcode;
+
+// 1108. Defanging an IP Address
+// https://leetcode.com/problems/defanging-an-ip-address/
+public class Solution1108 {
+    public String defangIPaddr(String address) {
+
+        StringBuilder sb = new StringBuilder();
+
+        for(char ch : address.toCharArray()){
+            if(ch == '.'){
+                sb.append("[.]");
+            }else{
+                sb.append(ch);
+            }
+        }
+
+        return sb.toString();
+    }
+
+    public static void main(String[] args) {
+
+        String address = "1.1.1.1";
+
+        Solution1108 sol = new Solution1108();
+        System.out.println(sol.defangIPaddr(address));
+    }
+}
---
+++ src/leetcode/Solution771.java
@@ -0,0 +1,38 @@
+package leetcode;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+// 771. Jewels and Stones
+// https://leetcode.com/problems/jewels-and-stones/
+public class Solution771 {
+    public int numJewelsInStones(String J, String S) {
+
+        int answer = 0;
+
+        Set<Character> set = new HashSet<Character>();
+
+        for(char ch : J.toCharArray()){
+            set.add(ch);
+        }
+
+        for(char ch : S.toCharArray()){
+            if(set.contains(ch)){
+               answer++;
+            }
+        }
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+
+        String J = "aA";
+        String S = "aAAbbbb";
+
+        Solution771 sol = new Solution771();
+        System.out.println(sol.numJewelsInStones(J,S));
+    }
+}
