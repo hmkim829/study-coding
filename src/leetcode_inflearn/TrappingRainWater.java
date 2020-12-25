d80827232a124e2b9235c38581f0ae64fedeba3a
hmkim829 <hmkim829@gmail.com>
2020-12-25 21:26:24 +0900

    42. Trapping Rain Water

---
+++ src/leetcode_inflearn/TrappingRainWater.java
@@ -0,0 +1,20 @@
+package leetcode_inflearn;
+
+// 42. Trapping Rain Water
+// https://leetcode.com/problems/trapping-rain-water/
+public class TrappingRainWater {
+
+    public static void main(String[] args) {
+
+        int[] height = {0,1,0,2,1,0,1,3,2,1,2,1};
+        TrappingRainWater sol = new TrappingRainWater();
+        System.out.println(sol.trap(height));
+    }
+
+    public int trap(int[] height) {
+
+
+
+        return 0;
+    }
+}
