22f8c62fb84fc60bfd855c5dd262ea09115a74cd
hmkim829 <hmkim829@gmail.com>
2024-03-20 08:25:14 +0900

    2/28 Unique Email Addresses 풀이 정리 2

--- src/leetcode/chapter01string/Q01UniqueEmailAddresses.java
+++ src/leetcode/chapter01string/Q01UniqueEmailAddresses.java
@@ -14,38 +14,14 @@ public class Q01UniqueEmailAddresses {
         String[] emails = {"test.email+alex@leetcode.com","test.email.leet+alex@code.com"};
 
         Q01UniqueEmailAddresses uea = new Q01UniqueEmailAddresses();
-        System.out.println(uea.mySolve(emails));
         System.out.println(uea.solve(emails));
-    }
-
-    // 24.02.28 풀이
-    public int mySolve(String[] emails) {
-
-        Set<String> emailSet = new HashSet<>();
-
-        for(String email : emails){
-            String[] splitEmail = email.split("@");
-            String localName = splitEmail[0];
-            String domain = splitEmail[1];
-
-            if(localName.indexOf(".") > -1) {
-                localName = localName.replace(".", "");
-            }
-
-            if(localName.indexOf("+") > -1) {
-                localName = localName.substring(0,localName.indexOf("+"));
-            }
-            emailSet.add(localName+"@"+domain);
-        }
-        return emailSet.size();
     }
 
     public int solve(String[] emails) {
 
-        Set<String> set = new HashSet<String>();
+        Set<String> set = new HashSet<>();
 
         for(String email : emails){
-
             String localName = makeLocalName(email);
             String domainName = makeDomainName(email);
 
@@ -69,7 +45,7 @@ public class Q01UniqueEmailAddresses {
                 continue;
             }
 
-            if(email.charAt(i) == '+' || email.charAt(i) == '+'){
+            if(email.charAt(i) == '+'){
                 break;
             }
 
