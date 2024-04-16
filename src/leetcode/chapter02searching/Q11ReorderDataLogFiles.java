e0a3df2c98e91d01941ead8fbe0df78dc399769d
hmkim829 <hmkim829@gmail.com>
2024-04-16 22:26:43 +0900

    Reorder Data in Log Files 풀이

--- src/leetcode/chapter02searching/Q11ReorderDataLogFiles.java
+++ src/leetcode/chapter02searching/Q11ReorderDataLogFiles.java
@@ -1,5 +1,7 @@
 package leetcode.chapter02searching;
 
+import java.util.Arrays;
+
 // 937. Reorder Data in Log Files
 // https://leetcode.com/problems/reorder-data-in-log-files/description/
 public class Q11ReorderDataLogFiles {
@@ -20,7 +22,31 @@ public class Q11ReorderDataLogFiles {
 
     public String[] reorderLogFiles(String[] logs) {
 
-        return null;
+        Arrays.sort(logs, (a, b) -> {
+
+            String[] splitA = a.split(" ", 2);
+            String[] splitB = b.split(" ", 2);
+
+            boolean isDigitA = Character.isDigit(splitA[1].charAt(0));
+            boolean isDigitB = Character.isDigit(splitB[1].charAt(0));
+
+            if(isDigitA && isDigitB) { // 숫자, 숫자 -> 그대로
+                return 0;
+            } else if(isDigitA && !isDigitB) { // 숫자, 문자 -> 내림
+                return 1;
+            } else if(!isDigitA && isDigitB) { // 문자, 숫자
+                return -1;
+            } else { // 문자, 문자
+                int compare = splitA[1].compareTo(splitB[1]);
+                if(compare == 0) {
+                    return splitA[0].compareTo(splitB[0]);
+                }else{
+                    return compare;
+                }
+            }
+        });
+
+        return logs;
     }
 
     public static void print(String[] logs) {
